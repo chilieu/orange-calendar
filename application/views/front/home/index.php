c5b525a79260e63d146377128bf6040812a35a45
Chi LT <lieu.thanh.chi@gmail.com>
2016-03-01 05:24:51 +0000

    u

--- application/views/front/home/index.php
+++ application/views/front/home/index.php
@@ -1,1 +1,90 @@
-index.php
+<div class="row">
+
+	<div class="col-md-10 col-md-offset-1">
+
+
+<form>
+
+  <fieldset>
+  	<legend>Contact Info</legend>
+
+	<div class="form-group col-md-6">
+	    <label for="exampleInputEmail1">First Name</label>
+	    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="John">
+  	</div>
+
+	<div class="form-group col-md-6">
+	    <label for="exampleInputEmail1">Last Name</label>
+	    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Doe">
+  	</div>
+
+	<div class="form-group col-md-12">
+	    <small class="text-muted">We'll never share your email and phone number with anyone else.</small>
+  	</div>
+
+  	<div class="form-group col-md-12">
+	    <label for="exampleInputEmail1">Email address</label>
+	    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="YourEmail@example.com">
+  	</div>
+
+  	<div class="form-group col-md-12">
+	    <label for="exampleInputEmail1">Phone</label>
+	    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="(xxx)-xxx-xxxx">
+  	</div>
+
+  </fieldset>
+
+  <fieldset>
+
+  	<legend>Reservation</legend>
+
+  	<div class="form-group col-md-12">
+	    <label for="exampleInputEmail1">Event</label>
+	    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Lễ Phục Sinh - Easter Sunday - Sunrise Joint Service	">
+  	</div>
+
+	<div class="form-group col-md-6">
+	    <label for="exampleInputEmail1">Start</label>
+        <div class='input-group date' id='datetimepicker1'>
+            <input type='text' class="form-control" />
+            <span class="input-group-addon">
+                <span class="glyphicon glyphicon-calendar"></span>
+            </span>
+        </div>
+  	</div>
+
+	<div class="form-group col-md-6">
+	    <label for="exampleInputEmail1">End</label>
+        <div class='input-group date' id='datetimepicker2'>
+            <input type='text' class="form-control" />
+            <span class="input-group-addon">
+                <span class="glyphicon glyphicon-calendar"></span>
+            </span>
+        </div>
+  	</div>
+
+  	<div class="form-group col-md-12">
+	    <label for="exampleInputEmail1">Room</label>
+	    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Room 11">
+  	</div>
+
+
+
+  	<div class="form-group col-md-12">
+  		<button type="submit" class="btn btn-primary">Submit</button>
+  	</div>
+
+  </fieldset>
+
+
+</form>
+
+	</div>
+
+</div>
+
+<script type="text/javascript">
+    $(function () {
+        $('#datetimepicker1').datetimepicker();
+    });
+</script>
--- application/views/front/layout.php
+++ application/views/front/layout.php
@@ -1,1 +1,44 @@
-<?=@$_body?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css" >
+
+
+    <!-- jQuery first, then Bootstrap JS. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="//v4-alpha.getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="/public/themes/bootstrap4/js/tether.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+  </head>
+  <body>
+
+    <nav class="navbar">
+      <a class="navbar-brand" href="#">OC</a>
+      <ul class="nav navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Reservation <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+
+    <div class="container">
+
+	<?=@$_body?>
+
+    </div><!-- /.container -->
+
+  </body>
+</html>
